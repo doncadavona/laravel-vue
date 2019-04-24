3b1a09700e0e3395fd938d1fb89795d97622fea5
Don Cadavona <dcadavona@gmail.com>
2019-04-25 01:41:27 +0800

    Updated User model factory; Seed users table.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\User;
-use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,14 +13,21 @@ use Faker\Generator as Faker;
 | your application. Factories provide a convenient way to generate new
 | model instances for testing / seeding your application's database.
 |
-*/
+ */
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'email'                => $faker->unique()->safeEmail,
+
+        'username'             => $faker->unique()->userName,
+        'first_name'           => $faker->firstName,
+        'last_name'            => $faker->lastName,
+        'address'              => $faker->address,
+        'postcode'             => $faker->postcode,
+        'contact_phone_number' => $faker->phoneNumber,
+
+        'email_verified_at'    => now(),
+        'password'             => bcrypt('secret'),
+        'remember_token'       => Str::random(10),
     ];
 });
